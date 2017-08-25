ffc67205a501571d41848de6e38f7abc8e7025d3
Tony Storti <tonystorti@gmail.com>
2017-08-25 09:44:47 -0500

    added CORS support

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//allow CORS requests (for testing purposes);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // //use logic and css files linked on main.handlebars
 app.use(express.static(__dirname + '/'));
 
